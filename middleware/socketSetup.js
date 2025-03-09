94b9c78c3f49d14a57d87001e02bef7b632ca90c
htkael <htkael@gmail.com>
2025-03-08 19:19:32 -0600

    CORS edit

--- middleware/socketSetup.js
+++ middleware/socketSetup.js
@@ -8,7 +8,7 @@ const prisma = require("../prisma/client");
 function setupSocketIO(server) {
   const io = new Server(server, {
     cors: {
-      origin: process.env.FRONTEND_URL || "http://localhost:3000",
+      origin: process.env.FRONTEND_URL || "http://localhost:5173",
       methods: ["GET", "POST"],
       credentials: true,
     },
