282f2d9253ea53d5a61ad19c58278ea243e602ed
htkael <htkael@gmail.com>
2025-03-12 15:41:11 -0500

    CORS

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin:
-      process.env.FRONTEND_URL ||
-      "http://localhost:5173" ||
+    origin: [
+      process.env.FRONTEND_URL,
+      "http://localhost:5173",
       "http://localhost:5174",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
